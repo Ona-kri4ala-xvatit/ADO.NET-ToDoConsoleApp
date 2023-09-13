06a7a8fd27eddd62876d22c4eb4b2b6e7faf42c4
Nijat Muradov <live4ever4@gmail.com>
2023-09-13 22:27:42 +0400

    Add project files.

---
+++ ToDoConsoleAppWithADO.NET/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ToDoConsoleAppWithADO.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string server = "localhost\\SQLEXPRESS";
+            const string database = "ToDoListDB";
+            const string connectionString = $"Server = {server}; Database = {database}; Integrated Security=True;TrustServerCertificate=True";
+
+            var connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+                var commandText = "Select * from Tasks";
+                var command = new SqlCommand(commandText, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID: {reader[0]}\nTask: {reader[1]}\n");
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
