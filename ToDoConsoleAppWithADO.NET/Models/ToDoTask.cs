e4d43ab5e3748f03676eea3bb4ed9afcc242abe0
Nijat Muradov <live4ever4@gmail.com>
2023-09-14 22:44:01 +0400

    Add Create, Read and Delete operations

---
+++ ToDoConsoleAppWithADO.NET/Models/ToDoTask.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoConsoleAppWithADO.NET.Models
+{
+    public class ToDoTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}\nTask: {Name}\n";
+        }
+    }
+}
--- ToDoConsoleAppWithADO.NET/Program.cs
+++ ToDoConsoleAppWithADO.NET/Program.cs
@@ -1,34 +1,74 @@
 ﻿using Microsoft.Data.SqlClient;
+using ToDoConsoleAppWithADO.NET.Models;
 
-namespace ToDoConsoleAppWithADO.NET
+const string server = "localhost\\SQLEXPRESS";
+const string database = "ToDoListDB";
+const string connectionString =
+    $"Server = {server}; Database = {database}; Integrated Security=True; TrustServerCertificate=True;";
+
+var tasks = new List<ToDoTask>();
+
+using (SqlConnection connection = new SqlConnection(connectionString))
 {
-    internal class Program
+    try
     {
-        static void Main(string[] args)
+        connection.Open();
+        while (true)
         {
-            const string server = "localhost\\SQLEXPRESS";
-            const string database = "ToDoListDB";
-            const string connectionString = $"Server = {server}; Database = {database}; Integrated Security=True;TrustServerCertificate=True";
+            Console.WriteLine($"1\tShow Tasks\n2\tCreate Task\n3\tUpdate Task\n4\tDelete Task\nEsc\tExit");
 
-            var connection = new SqlConnection(connectionString);
-
-            try
+            switch (Console.ReadKey().Key)
             {
-                connection.Open();
+                case ConsoleKey.D1:
+                    Console.Clear();
 
-                var commandText = "Select * from Tasks";
-                var command = new SqlCommand(commandText, connection);
-                SqlDataReader reader = command.ExecuteReader();
+                    CrudOperations.ReadTasks(connectionString, tasks);
 
-                while (reader.Read())
-                {
-                    Console.WriteLine($"ID: {reader[0]}\nTask: {reader[1]}\n");
-                }
-            }
-            catch (SqlException e)
-            {
-                Console.WriteLine(e.Message);
+                    foreach (var item in tasks)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    break;
+
+                case ConsoleKey.D2:
+                    Console.Clear();
+
+                    Console.Write("Enter task: ");
+                    string tempTaskName = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(tempTaskName))
+                        continue;
+
+                    CrudOperations.CreateTask(connectionString, new ToDoTask() { Name = tempTaskName });
+                    Console.WriteLine("Task created\n\n");
+                    break;
+
+                case ConsoleKey.D3:
+                    Console.Clear();
+
+
+
+                    break;
+
+                case ConsoleKey.D4:
+                    Console.Clear();
+
+                    Console.Write("Enter task id: ");
+                    int tempTaskId = int.Parse(Console.ReadLine());
+
+                    CrudOperations.DeleteTask(connectionString, tempTaskId);
+                    Console.WriteLine("Task deleted\n\n");
+                    break;
+
+                case ConsoleKey.Escape:
+                    Environment.Exit(0);
+                    break;
             }
         }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
     }
 }
